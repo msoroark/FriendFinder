846911e9087d0dd2667b1e23598132728a65dea2
Meg O'Roark <msoroark@gmail.com>
2017-09-22 21:48:00 -0500

    first commit, submit button not working

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+//set up code for express
+var app = express();
+var PORT = process.env.PORT || 8080;
+ 
+
+//Set up code for body-parser
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+require('./app/routing/apiRoutes.js')(app);
+
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT: " + PORT);
+});
